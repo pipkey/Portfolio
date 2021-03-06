615ee31a0959c6646a1f1a93d312da056142b795
pipkey <galagen2014@gmail.com>
2021-03-06 16:08:00 +0300

    all block but not photo

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,19 +1,40 @@
 import React from "react";
-import style from "./Footer.module.css"
+import style from "./Footer.module.scss"
 import Title from "../common/Components/Title";
+import vk from "../common/pict/vkontacte.png"
+import inst from "../common/pict/instagram.png"
+import telega from "../common/pict/telegram.png"
+import twit from "../common/pict/twittew.png"
 
-const Footer =()=> {
-    return(
+
+const Footer = () => {
+
+    let vkIcon = {
+        backgroundImage: `url( ${vk} )`,
+    };
+    let instIcon = {
+        backgroundImage: `url( ${inst} )`,
+    };let tgIcon = {
+        backgroundImage: `url( ${telega} )`,
+    };let twitIcon = {
+        backgroundImage: `url( ${twit} )`,
+    };
+
+    return (
         <div className={style.footer}>
             <Title title={'Let’s Talk!'}/>
             <div className={style.hrefs}>
-                <span>vk</span>
-                <hr/>
-                <span>Inst</span>
-                <hr/>
-                <span>TG</span>
-                <hr/>
-                <span>twitter</span>
+                <button style={vkIcon}> </button>
+                <button style={instIcon}> </button>
+                <button style={tgIcon}> </button>
+                <button style={twitIcon}> </button>
+                {/*<span>vk</span>*/}
+                {/*<hr/>*/}
+                {/*<span>Inst</span>*/}
+                {/*<hr/>*/}
+                {/*<span>TG</span>*/}
+                {/*<hr/>*/}
+                {/*<span>twitter</span>*/}
             </div>
         </div>
     )
--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,24 +1,26 @@
 import React from "react"
-import style from "./Form.module.css"
-import container from "../common/styles/container.module.css"
+import style from "./Form.module.scss"
 import Title from "../common/Components/Title";
 
 
 const Form = () => {
 
+
+
     return (
         <div className="bigDiv">
-            <div className={style.mainCont} >
-                    <Title title={"You can write me"}/>
+            <div  className={style.mainCont}>
+                <Title title={"You can write me"}/>
+                <div className={style.inputAreas}>
+                    <input className={style.inp} type="text" placeholder={"Enter your name"}/>
+                    <input className={style.inp} type="text" placeholder={"Enter your phone"}/>
+                    <input className={style.inp} type={"text"} placeholder={"Enter your Email"}/>
 
-                <div className={` ${style.inputAreas} ${container}`}>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <input type="text"/>
                     <button className={style.btn}> Send</button>
                 </div>
 
 
+                <div className={style.free}>I'm available for Freelance</div>
             </div>
         </div>
     )
