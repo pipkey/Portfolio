ea323eda51d299eecd2e15cb1d758543bcdd6867
pipkey <galagen2014@gmail.com>
2021-04-19 00:15:53 +0300

    footer clicked

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,37 +1,39 @@
 import React from "react";
 import style from "./Footer.module.scss"
 import Title from "../common/Components/Title";
-import vk from "../common/pict/vkontacte.png"
+import FB from "../common/pict/Facebook.png"
 import inst from "../common/pict/instagram.png"
 import telega from "../common/pict/telegram.png"
-import twit from "../common/pict/twittew.png"
+import linkedIN from "../common/pict/linkedIn.png"
 import Fade from 'react-reveal/Fade';
 
 
 const Footer = () => {
 
-    let vkIcon = {
-        backgroundImage: `url( ${vk} )`,
-    };
-    let instIcon = {
-        backgroundImage: `url( ${inst} )`,
-    };
-    let tgIcon = {
-        backgroundImage: `url( ${telega} )`,
-    };
-    let twitIcon = {
-        backgroundImage: `url( ${twit} )`,
-    };
 
     return (
         <div id={"contacts"} className={style.footer}>
             <Title title={'Let’s Talk!'}/>
             <Fade right>
                 <footer className={style.hrefs}>
-                    <button style={vkIcon}> </button>
+                    {/* <button style={FaceIcon}> <a href="https://www.facebook.com/pil.kav.5"></a> </button>
                     <button style={instIcon}> </button>
                     <button style={tgIcon}> </button>
-                    <button style={twitIcon}> </button>
+                    <button style={linkedicon}> </button> */}
+
+                    <a href="https://www.facebook.com/pil.kav.5" target="blank">
+                        <img src={FB} alt="facebook"/>
+                    </a>
+                    <a href="https://www.instagram.com/____p1p_key____/" target="blank">
+                        <img src={inst} alt="instagram"/>
+                    </a>
+                    <a href="http://tele.click/@pipkey" target="blank">
+                        <img src={telega} alt="telegram"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/philip-kamenkov-01bb19209/" target="blank">
+                        <img src={linkedIN} alt="linkedIn"/>
+                    </a>
+
                 </footer>
             </Fade>
         </div>
