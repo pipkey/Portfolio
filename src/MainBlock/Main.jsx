306f741d3414689de42f1d2204e241e5e8a18118
pipkey <galagen2014@gmail.com>
2021-04-19 00:52:10 +0300

    add download CV

--- src/MainBlock/Main.jsx
+++ src/MainBlock/Main.jsx
@@ -3,6 +3,7 @@ import style from "./Main.module.scss"
 import wall from "./../assets/img/wall.jpg"
 import ReactTypingEffect from 'react-typing-effect';
 import myPhoto from '../assets/img/vXJ8pceZqtE.jpg'
+import Button from "../common/Components/Button";
 
 const Main = () => {
 
@@ -17,12 +18,17 @@ const Main = () => {
                 <div className={style.specText}>
                     <ReactTypingEffect text={["Hi there", "Nice to meet you"]} typingDelay={1000} cursor="!"
                         speed={400} />
+
                 </div>
                 <div className={style.text}>
                     I'm Philip Kamenkov -
                     <br />
                     Frontend Developer
+                    <a href="https://yadi.sk/i/2TbTn6GULAnufw" download="my CV" target="blank">
+                        <Button title="Download CV" />
+                    </a>
                 </div>
+
             </div>
             <div className={style.img}>
                 <img className={style.photo} src={myPhoto} alt="picture" />
